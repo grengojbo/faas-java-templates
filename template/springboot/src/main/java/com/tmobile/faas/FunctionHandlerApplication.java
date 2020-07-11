c358e0b5dca8d1664d96fa3dd4af097a4456daa8
Peidong Hu <peidong.hu@arris.com>
2020-07-10 21:19:59 -0500

    enable @EnableAutoConfiguration

--- template/springboot/src/main/java/com/tmobile/faas/FunctionHandlerApplication.java
+++ template/springboot/src/main/java/com/tmobile/faas/FunctionHandlerApplication.java
@@ -22,7 +22,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@ComponentScan
 public class FunctionHandlerApplication {
 
 	public static void main(String[] args) {
