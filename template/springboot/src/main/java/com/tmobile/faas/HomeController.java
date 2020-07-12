60539db34000981c76a97e23fa15c7c620c2bee5
Peidong Hu <peidong.hu@arris.com>
2020-07-12 14:04:05 -0500

    springboot 2.3.1 and lazy-initialization

--- template/springboot/src/main/java/com/tmobile/faas/HomeController.java
+++ template/springboot/src/main/java/com/tmobile/faas/HomeController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.validation.Valid;
+
 //import function.Handler;
 
 @Controller
@@ -35,7 +35,7 @@ public class HomeController {
    
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
-    public ResponseEntity<String> handle(@RequestBody @Valid byte[] payload) {
+    public ResponseEntity<String> handle(@RequestBody byte[] payload) {
         String response = handler.handle(payload);
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
