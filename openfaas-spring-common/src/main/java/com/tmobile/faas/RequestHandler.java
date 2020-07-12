3675cd3e8aad1e4311795241f90d158688ce693d
Peidong Hu <peidong.hu@arris.com>
2020-07-12 01:52:01 -0500

    add readSecret function in common interface

--- openfaas-spring-common/src/main/java/com/tmobile/faas/RequestHandler.java
+++ openfaas-spring-common/src/main/java/com/tmobile/faas/RequestHandler.java
@@ -1,8 +1,37 @@
 package com.tmobile.faas;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public interface RequestHandler {
 
-    String handle(byte[] requestPayload);
+	String handle(byte[] requestPayload);
+
+	String handle();
+
+	public static String readSecret(String key) {
+		String basePath = "/var/openfaas/secrets/";
+		if (!System.getenv("secret_mount_path").isEmpty()) {
+			basePath = System.getenv("secret_mount_path");
+		}
+
+		String readPath = basePath + key;
+		String content = "";
+		 
+        try
+        {
+            content = new String ( Files.readAllBytes( Paths.get(readPath) ) );
+        } 
+        catch (IOException e) 
+        {
+            e.printStackTrace();
+            return "";
+        }
  
-    String handle();
+        return content;
+
+		
+
+	}
 }
---
+++ openfaas-spring-common/src/test/java/com/tmobile/faas/RequestHanlderTest.java
@@ -0,0 +1,54 @@
+package com.tmobile.faas;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Field;
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import com.tmobile.faas.RequestHandler;
+
+class RequestHanlderTest {
+	
+	protected static void setEnv(Map<String, String> newenv) throws Exception {
+		  try {
+		    Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+		    Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+		    theEnvironmentField.setAccessible(true);
+		    Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+		    env.putAll(newenv);
+		    Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+		    theCaseInsensitiveEnvironmentField.setAccessible(true);
+		    Map<String, String> cienv = (Map<String, String>)     theCaseInsensitiveEnvironmentField.get(null);
+		    cienv.putAll(newenv);
+		  } catch (NoSuchFieldException e) {
+		    Class[] classes = Collections.class.getDeclaredClasses();
+		    Map<String, String> env = System.getenv();
+		    for(Class cl : classes) {
+		      if("java.util.Collections$UnmodifiableMap".equals(cl.getName())) {
+		        Field field = cl.getDeclaredField("m");
+		        field.setAccessible(true);
+		        Object obj = field.get(env);
+		        Map<String, String> map = (Map<String, String>) obj;
+		        map.clear();
+		        map.putAll(newenv);
+		      }
+		    }
+		  }
+		}
+	
+	@Test
+	void test() {
+		Collections.singletonMap("secret_mount_path", ".");
+
+		try {
+			setEnv(Collections.singletonMap("secret_mount_path", System.getProperty("user.dir") + "/"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(RequestHandler.readSecret("test"));
+	}
+
+}
